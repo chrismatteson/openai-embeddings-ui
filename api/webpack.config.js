f329219e112a501e1ee9abe11781e4ca073e31b3
Justin Pflueger <justin.pflueger@fermyon.com>
2023-06-06 16:37:08 -0600

    configure webpack to use polyfills

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.ts',
+    target: 'es6',
     module: {
         rules: [
             {
@@ -11,13 +12,37 @@ module.exports = {
             },
         ],
     },
+    externalsType: 'global',
+    externals: {
+        "fetch": "fetch",
+        "fsPromises": "fsPromises",
+        "glob": "glob",
+        "crypto": "crypto",
+    },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
+        fallback: {
+            // polyfills for nodejs modules
+            "url": require.resolve("url/"),
+            "util": require.resolve("util/"),
+            "buffer": require.resolve("buffer/"),
+            "path": require.resolve("path-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "https": require.resolve("https-browserify"),
+            "http": require.resolve("stream-http"),
+            // no polyfill for these
+            "fs": false,
+        },
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'spin.js',
-        library: 'spin'
+        chunkFormat: 'module',
+        chunkLoading: 'import',
+        library: {
+            type: 'this',
+            name: 'spin',
+        },
     },
     optimization: {
         minimize: false
